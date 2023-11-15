d887941a4686a6409ade84f22f079b167fe66bbe
Yadija <ayuliadi01@gmail.com>
2023-11-15 08:52:05 +0800

    ⚒️build: make conditional when delete transaction

--- public/script/script.js
+++ public/script/script.js
@@ -204,7 +204,7 @@ const main = () => {
     const h2 = document.createElement('h2');
     h2.innerText = 'Detail History';
     h2.className = 'modal__title';
-  
+
     const closeButton = document.createElement('button');
     closeButton.className = 'modal__close';
     closeButton.innerHTML = '&times;';
@@ -213,53 +213,53 @@ const main = () => {
       const modal = document.getElementById('detailModalContainer');
       modal.remove();
     }
-  
+
     const modalHeader = document.createElement('section');
     modalHeader.className = 'modal__header';
     modalHeader.append(h2, closeButton);
-  
+
     // modal body
     const paraDate = document.createElement('p');
     paraDate.className = 'modal__body-date';
     paraDate.innerText = date;
-  
+
     const paraType = document.createElement('p');
     paraType.className = 'modal__body-type';
     paraType.innerHTML = `<strong>Type:</strong> ${type}`;
-  
+
     const paraCategory = document.createElement('p');
     paraCategory.className = 'modal__body-category';
     paraCategory.innerHTML = `<strong>Category:</strong> ${category}`;
-  
+
     const paraAmount = document.createElement('p');
     paraAmount.className = 'modal__body-amount';
     paraAmount.innerHTML = `<strong>Amount:</strong> ${amount}`;
-  
+
     const paraDescription = document.createElement('p');
     paraDescription.className = 'modal__body-description';
     paraDescription.innerHTML = `<strong>Description:</strong> ${description}`;
-  
+
     const modalBody = document.createElement('section');
     modalBody.className = 'modal__body';
     modalBody.append(paraDate, paraType, paraCategory, paraAmount, paraDescription);
-  
+
     // modal footer
     const deleteButton = document.createElement('button');
     deleteButton.className = 'modal__footer-button-delete';
     deleteButton.innerText = 'Delete';
     deleteButton.onclick = () => {
-      window.location.href = `http://${window.location.host}/delete/${id}`;
+      alertModal(id);
     }
-  
+
     const modalFooter = document.createElement('section');
     modalFooter.className = 'modal__footer';
     modalFooter.append(deleteButton);
-  
+
     // modal
     const modal = document.createElement('section');
     modal.className = 'modal';
     modal.append(modalHeader, modalBody, modalFooter);
-  
+
     const modalContainer = document.createElement('section');
     modalContainer.id = 'detailModalContainer';
     modalContainer.className = 'detail-modal-container';
@@ -268,6 +268,54 @@ const main = () => {
         modalContainer.remove();
       }
     }
+    modalContainer.append(modal);
+
+    // add to body
+    const body = document.querySelector('body');
+    body.append(modalContainer);
+  }
+
+  const alertModal = (id) => {
+    // modal header
+    const h2 = document.createElement('h2');
+    h2.innerText = 'Do you want to delete this transaction?';
+    h2.className = 'modal__title';
+
+    const modalHeader = document.createElement('section');
+    modalHeader.className = 'modal__header';
+    modalHeader.append(h2);
+
+  
+    // modal button
+    const deleteButton = document.createElement('button');
+    deleteButton.className = 'modal__button-delete';
+    deleteButton.innerText = 'Delete';
+    deleteButton.onclick = () => {
+      window.location.href = `http://${window.location.host}/delete/${id}`;
+    }
+
+    const cancelButton = document.createElement('button');
+    cancelButton.className = 'modal__button-cancel';
+    cancelButton.innerText = 'Cancel';
+    cancelButton.onclick = () => {
+      const modalContainer = document.getElementById('alertModalContainer');
+      modalContainer.remove();
+    }
+  
+    const modalButton = document.createElement('section');
+    modalButton.className = 'modal__button';
+    modalButton.append(deleteButton, cancelButton);
+  
+    // modal
+    const modal = document.createElement('section');
+    modal.className = 'modal';
+    modal.style.width = '400px';
+    modal.append(modalHeader, modalButton);
+  
+    const modalContainer = document.createElement('section');
+    modalContainer.id = 'alertModalContainer';
+    modalContainer.className = 'modal-container';
+    modalContainer.style.display = 'block';
     modalContainer.append(modal);
   
     // add to body
